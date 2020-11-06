d21035b38b66eb65cf46e2589ab219e34990e5e7
mahdieh-ahmadi <m119.ahmadi@gmail.com>
2020-11-06 10:43:11 +0330

    mode1 finish

---
+++ string_detectore_mode1.v
@@ -0,0 +1,66 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    09:49:34 11/06/2020 
+// Design Name: 
+// Module Name:    string_detectore_mode1 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module string_detectore_mode1(
+    input [3:0] string1,
+    input string2,
+    input clk,
+    output reg [3:0] N
+    );
+	 
+	 reg start;
+	 reg [3:0] s;
+	 reg [4:0] counter;
+	 
+	 initial begin 
+		N <= 0;
+		counter <= 0 ;
+	end
+	 
+	
+
+always @(posedge clk) begin
+	
+	if (string2 !== 1'bx && counter < 20) begin
+		s[3] <= s[2];
+		s[2] <= s[1];
+		s[1] <= s[0];
+		s[0] <= string2;
+		counter <= counter +1 ;
+		
+		if (string1 == s) N <= N+1;
+	end
+	
+	if (counter  < 24 && counter > 19) begin
+		s[3] <= s[2];
+		s[2] <= s[1];
+		s[1] <= s[0];
+		s[0] <= string2;
+		counter <= counter +1 ;
+	end
+
+		if (counter == 23) begin
+			counter <= 0;
+			N <= 0;
+		end
+	
+end
+
+endmodule
---
+++ test-string_detectore_mode1.v
@@ -0,0 +1,84 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   10:23:02 11/06/2020
+// Design Name:   string_detectore_mode1
+// Module Name:   E:/university/3991/fpga/homeworks/string_detectore/test-string_detectore_mode1.v
+// Project Name:  string-detectore
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: string_detectore_mode1
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module test-string_detectore_mode1;
+
+	// Inputs
+	reg [3:0] string1;
+	reg string2;
+	reg clk;
+
+	// Outputs
+	wire [3:0] N;
+
+	// Instantiate the Unit Under Test (UUT)
+	string_detectore_mode1 uut (
+		.string1(string1), 
+		.string2(string2), 
+		.clk(clk), 
+		.N(N)
+	);
+
+	initial begin
+		// Initialize Inputs
+		//string1 = 0;
+		//string2 = 0;
+		clk = 0;
+
+		// Wait 100 ns for global reset to finish
+		#100;
+        
+		// Add stimulus here
+		string1 = 4'b0110;
+		
+		@(negedge clk) string2 <= 0;
+		@(negedge clk) string2 <= 1;
+		@(negedge clk) string2 <= 1;
+		@(negedge clk) string2 <= 0;
+		@(negedge clk) string2 <= 0;
+		
+		@(negedge clk) string2 <= 1;
+		@(negedge clk) string2 <= 1;
+		@(negedge clk) string2 <= 0;
+		@(negedge clk) string2 <= 0;
+		@(negedge clk) string2 <= 0;
+		
+		@(negedge clk) string2 <= 0;
+		@(negedge clk) string2 <= 1;
+		@(negedge clk) string2 <= 1;
+		@(negedge clk) string2 <= 0;
+		@(negedge clk) string2 <= 1;
+		
+		@(negedge clk) string2 <= 1;
+		@(negedge clk) string2 <= 0;
+		@(negedge clk) string2 <= 0;
+		@(negedge clk) string2 <= 0;
+		@(negedge clk) string2 <= 1;
+
+	end
+	
+	always #50 clk = ~clk;
+      
+endmodule
+
